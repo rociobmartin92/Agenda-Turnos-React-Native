28ed56718ee23561e530ad76fa14837929819ad0
Rocio <martinrocio.1992@gmail.com>
2022-02-08 12:43:10 -0300

    card finished

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,12 @@ import {
   Center,
   Stack,
   Heading,
-  Text,
   HStack,
   Image,
   AspectRatio,
+  Button,
 } from 'native-base';
+import {Text, StyleSheet} from 'react-native';
 
 const Card = () => {
   const {name, date, phone, job} = useContext(TurnoContext);
@@ -43,47 +44,55 @@ const Card = () => {
       monthName[US_date.getMonth()]
     );
   };
+
   return (
     <Box alignItems="center" marginTop={10}>
-      <Box
-        maxW="80"
-        rounded="lg"
-        overflow="hidden"
-        borderColor="coolGray.200"
-        borderWidth="1">
-        <Box>
-          <AspectRatio w="100%" ratio={16 / 10}>
-            <Image
-              source={{
-                uri: 'https://media.istockphoto.com/photos/white-soft-wool-texture-background-cotton-wool-light-natural-sheep-picture-id942216898?k=6&m=942216898&s=170667a&w=0&h=Cw8WkUyiwviND6h6-avsYYw84FbahZlZV3SGuols-k8=',
-              }}
-              alt="image"
-            />
-          </AspectRatio>
-          <Center position="absolute" bg="white" bottom="0" px="3" py="3">
-            <Text marginTop={2} fontSize="lg">
-              {`Nombre: ${name}`}
-            </Text>
-            <Stack p="2" space={3}>
-              <Stack space={2}>
-                <Heading size="lg">
-                  <Text> {job}</Text>
-                </Heading>
-              </Stack>
-              <Text fontWeight="400">{`${formater(
-                date,
-              )} - ${date.toLocaleTimeString()} `}</Text>
-              <HStack space={4}>
-                <HStack>
-                  <Text fontWeight="400">{`Teléfono: ${phone}`}</Text>
+      <Button margin={0} padding={1} bg="#dea5a4" _pressed={{bg: 'rgb(0,0,0)'}}>
+        <Box
+          maxW="80"
+          rounded="lg"
+          overflow="hidden"
+          borderColor="coolGray.200"
+          borderWidth="1">
+          <Box>
+            <AspectRatio w="100%" ratio={16 / 10}>
+              <Image
+                source={{
+                  uri: 'https://media.istockphoto.com/photos/white-soft-wool-texture-background-cotton-wool-light-natural-sheep-picture-id942216898?k=6&m=942216898&s=170667a&w=0&h=Cw8WkUyiwviND6h6-avsYYw84FbahZlZV3SGuols-k8=',
+                }}
+                alt="image"
+              />
+            </AspectRatio>
+            <Center position="absolute" bg="white" bottom="0" px="5" py="7">
+              <Text style={estilo.name}>{name}</Text>
+              <Stack p="2" space={3}>
+                <Stack space={2}>
+                  <Heading size="lg">
+                    <Text style={estilo.job}> {job}</Text>
+                  </Heading>
+                </Stack>
+                <Text style={estilo.date}>{`${formater(
+                  date,
+                )} - ${date.toLocaleTimeString()} `}</Text>
+                <HStack space={4}>
+                  <HStack>
+                    <Text style={estilo.phone}>{phone}</Text>
+                  </HStack>
                 </HStack>
-              </HStack>
-            </Stack>
-          </Center>
+              </Stack>
+            </Center>
+          </Box>
         </Box>
-      </Box>
+      </Button>
     </Box>
   );
 };
+
+const estilo = StyleSheet.create({
+  name: {fontSize: 20, fontWeight: 'bold', color: 'rgb(0, 0, 0)'},
+  job: {fontSize: 21, fontWeight: 'bold', color: '#dea5a4'},
+  date: {fontSize: 15, fontWeight: 'bold', color: 'rgb(0, 0, 0)'},
+  phone: {fontSize: 21, fontWeight: 'bold'},
+});
 
 export default Card;
