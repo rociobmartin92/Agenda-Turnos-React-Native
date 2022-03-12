3479b5d70e59db66a5fd7bda7db31beab113a1e1
Rocio Martin <martinrocio.1992@gmail.com>
2022-03-12 18:38:22 -0300

    CA

--- src/components/stack/Album.js
+++ src/components/stack/Album.js
@@ -1,17 +1,27 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ImagePicker from 'react-native-image-crop-picker';
+import {FlatList, StyleSheet, Text} from 'react-native';
 import {Box, Image, Button} from 'native-base';
 import plus from '../../assets/images/plus.jpg';
 
+const fotos = [
+  {nombre: 'Rocio', situacion: 'Totalmente enamorado'},
+  {nombre: 'Belen', situacion: 'No puedo sentir nada'},
+];
+
 const Album = () => {
+  // const [foto, setFoto] = useState([]);
+
+  // eslint-disable-next-line react/prop-types
+
   const openCamera = () => {
     ImagePicker.openPicker({
-      width: 300,
+      width: 400,
       height: 400,
       cropping: true,
     }).then(image => {
-      console.log(image);
+      console.log(image.path);
     });
   };
 
@@ -26,6 +36,21 @@ const Album = () => {
         _pressed={{bg: 'transparent'}}>
         <Image source={plus} alt="Agregar Foto" size={16} />
       </Button>
+      <Box>
+        <FlatList
+          ListEmptyComponent={<Text>No hay fotos para mostrar</Text>}
+          data={fotos}
+          renderItem={({item}) => {
+            return (
+              <Box>
+                <Text> {item.nombre} </Text>
+                <Text> {item.situacion} </Text>
+              </Box>
+            );
+          }}
+          keyExtractor={item => item.nombre}
+        />
+      </Box>
     </Box>
   );
 };
